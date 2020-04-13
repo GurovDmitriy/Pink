72160c538b013a0fe6c7cf0cdad5c38e4423f081
GurovDmitriy <gurovdmitriy1991@gmail.com>
2020-04-13 09:46:56 +0300

    Тестирование базовой авоматизации сборки

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,52 @@ module.exports = function(grunt) {
   require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
+    svgstore: {
+      options: {
+        includeTitleElement: false
+      },
+      sprite: {
+        files: {
+          "source/image/min/sprite.svg": ["source/image/*-icon.svg"]
+        }
+      }
+    },
+    cwebp: {
+      images: {
+        options: {
+          q: 90
+        },
+        files: [{
+          expand: true,
+          cwd: "source/image/",
+          src: ["**/*.{png,jpg,gif}"],
+          dest: "source/image/min/"
+        }]
+      }
+    },
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+          },
+          files: [{
+            expand: true,
+            cwd: "source/image/",
+            src: ["**/*.{png,jpg,gif}"],
+            dest: "source/image/min/"
+            }]
+          }
+        },
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "source/css/style-min.css": ["source/css/style.css"]
+        }
+      }
+    },
     less: {
       style: {
         files: {
