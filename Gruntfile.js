614ff26b6ac7b83b2b55d29f5ec5fa209048d58e
GurovDmitriy <gurovdmitriy1991@gmail.com>
2020-04-30 23:52:15 +0300

    Графика декор Пинк

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,83 +1,148 @@
-"use strict";
-
 module.exports = function(grunt) {
   require("load-grunt-tasks")(grunt);
 
+
   grunt.initConfig({
-    svgstore: {
-      options: {
-        includeTitleElement: false
-      },
-      sprite: {
-        files: {
-          "source/image/min/sprite.svg": ["source/image/origin/*-icon.svg"]
+    watch: {
+      styleWatch: {
+        files: ["source/less/**/*.less"],
+        tasks: ["less", "postcss"]
+      }
+    },
+
+    browserSync: {
+      serverSync: {
+        bsFiles: {
+          src: ["source/*.html", "source/css/*.css"]
+        },
+        options: {
+          server: "source/",
+          watchTask: true
         }
       }
     },
-    cwebp: {
-      images: {
+
+    less: {
+      styleLess: {
         options: {
-          q: 90
+          relativeUrls: true
+        },
+        files: {
+          "source/css/style.css": "source/less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      stylePrefix: {
+        options: {
+          processors: [
+          require("autoprefixer")()
+        ]
+      },
+      src: "source/css/style.css"
+      }
+    },
+
+    csso: {
+      styleMin: {
+        options: {
+          report: "gzip"
+        },
+        expand: true,
+        cwd: "build/css/",
+        src: ["*.css"],
+        dest: "build/css/"
+      }
+    },
+
+    svgstore: {
+      options: {
+        includeTitleElement: false,
+        prefix : "icon-",
+        svg: {
+          viewBox : "0 0 100 100",
+          xmlns: "http://www.w3.org/2000/svg"
+        }
+      },
+      svgSprite: {
+        files: {
+          "source/image/min/sprite.svg": ["source/image/origin/*.svg"]
+        }
+      }
+    },
+
+    cwebp: {
+      imagesWebp: {
+        options: {
+          q: 70
         },
         files: [{
-          expand: true,
+          expand:true,
           cwd: "source/image/origin/",
           src: ["**/*.{png,jpg,gif}"],
           dest: "source/image/min/"
         }]
       }
     },
-    imagemin: {
-      images: {
+
+    image: {
+      imageMin: {
         options: {
-          optimizationLevel: 3
-          },
-          files: [{
-            expand: true,
-            cwd: "source/image/origin/",
-            src: ["**/*.{png,jpg,gif}"],
-            dest: "source/image/"
-            }]
-          }
+          optipng: ["-i 1", "-strip all", "-fix", "-o7", "-force"],
+          pngquant: ["--speed=1", "--force", 256],
+          zopflipng: ["-y", "--lossy_8bit", "--lossy_transparent"],
+          jpegRecompress: ["--strip", "--quality", "medium", "--min", 40, "--max", 80],
+          mozjpeg: ["-optimize", "-progressive"],
+          guetzli: ["--quality", 85],
+          gifsicle: ["--optimize"],
+          svgo: ["--enable", "cleanupIDs", "--disable", "convertColors"]
         },
-    csso: {
-      style: {
-        options: {
-          report: "gzip"
-        },
-        files: {
-          "source/css/style-min.css": ["source/css/style.css"]
-        }
+        files: [{
+          expand: true,
+          cwd: "source/image/origin/",
+          src: ["**/*.{png,jpg,gif,svg}"],
+          dest: "source/image/min/"
+        }]
       }
     },
-    less: {
-      style: {
-        files: {
-          "source/css/style.css": "source/less/style.less"
-        }
+
+    clean: {
+      buildClean: {
+        src: ["build/"]
       }
     },
-    watch: {
-      style: {
-        files: ["source/less/**/*"],
-        tasks: ["less"]
-      }
-    },
-    browserSync: {
-      server: {
-        bsFiles: {
-          src: ["source/*.html", "source/css/*.css"]
-        },
-        options: {
-          server: "source/",
-          watchTask: true,
-          notify: false,
-          open: true,
-          cors: true,
-          ui: false
-        }
+
+    copy: {
+      copyToBuild: {
+        expand: true,
+        cwd: "source/",
+        src: [
+          "fonts/**/*.{woff,woff2}",
+          "*.html",
+          "css/*.css",
+          "image/**/*",
+          "js/*"
+        ],
+        dest: "build/"
       }
     }
   });
-  grunt.registerTask("serve", ["browserSync", "watch"]);
+
+  grunt.registerTask("serve", [
+    "browserSync",
+    "watch"
+  ]);
+
+  grunt.registerTask("imgpress", [
+    "cwebp",
+    "image",
+    "svgstore"
+  ]);
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "csso"
+  ]);
 };
